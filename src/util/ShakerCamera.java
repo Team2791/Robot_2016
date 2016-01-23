6b739900f6a50a47e91596d67d22deec81f766cf
Akhil Jacob <akhilcjacob@gmail.com>
2016-01-23 08:46:33 -0500

    Changed camera code

--- src/util/ShakerCamera.java
+++ src/util/ShakerCamera.java
@@ -1,52 +1,66 @@
 package util;
 
+import com.ni.vision.NIVision;
 import configuration.Camera;
 import edu.wpi.first.wpilibj.CameraServer;
-import org.opencv.core.Mat;
-import org.opencv.core.Scalar;
-import org.opencv.core.Size;
-import org.opencv.imgproc.Imgproc;
+import edu.wpi.first.wpilibj.vision.AxisCamera;
 
-public class ShakerCamera {
-    private static int hueStart;
-    private static int saturationStart;
-    private static int valueStart;
-    private static int hueStop;
-    private static int saturationStop;
-    private static int valueStop;
-    private static Scalar minValues;
-    private static Scalar maxValues;
-    private static Mat HsvImage = new Mat();
-    private static Mat maskedImage = new Mat();
-    private static Mat outputImage = new Mat();
+import static com.ni.vision.NIVision.*;
+
+
+public class ShakerCamera extends AxisCamera {
+    private int hueStart;
+    private int saturationStart;
+    private int valueStart;
+    private int hueStop;
+    private int saturationStop;
+    private int valueStop;
     private CameraServer camServer;
+    private Image frame;
+    private int session;
+    private Range hue;
+    private Range sat;
+    private Range val;
+    private ParticleFilterCriteria2 criteria;
+    private ParticleFilterOptions options;
+    private ROI roi;
+
 
     public ShakerCamera() {
-        hueStart = Camera.hueLow; //put these values on smartdasboard --> possibly changeable via sliders
-        hueStop = Camera.hueHigh;
-        saturationStart = Camera.saturationLow;
-        saturationStop = Camera.saturationHigh;
-        valueStart = Camera.valueLow;
-        valueStop = Camera.valueHigh;
-        minValues = new Scalar(hueStart, saturationStart, valueStart);
-        maxValues = new Scalar(hueStop, saturationStop, valueStop);
-        // camServer = new CameraServer();
+        super("cam0");
+        this.hueStart = Camera.HUE_LOW; //put these values on smartdasboard --> possibly changeable via sliders
+        this.hueStop = Camera.HUE_HIGH;
+        this.saturationStart = Camera.SATURATION_LOW;
+        this.saturationStop = Camera.SATURATION_HIGH;
+        this.valueStart = Camera.VALUE_LOW;
+        this.valueStop = Camera.VALUE_HIGH;
+        this.hue = new Range(hueStart, hueStop);
+        this.sat = new Range(saturationStart, saturationStop);
+        this.val = new Range(valueStart, valueStop);
+        criteria = new ParticleFilterCriteria2(NIVision.MeasurementType.MT_AREA, Camera.minArea,
+                Camera.maxArea, Camera.calibrated, Camera.exclude);
+        options = new ParticleFilterOptions();
+        update();
+    }
 
+    public void update() {//send next camera image
+
+        super.getImage(frame);
+        //use colorizer.org
+        imaqColorThreshold(frame, frame, 1, ColorMode.HSL, hue, sat, val);
+        imaqParticleFilter3(frame, frame, criteria, 5, options, roi);
+        imaqConvexHull(frame, frame, 4);//int is unknown and never will be known
+        camServer.setImage(frame);
+
+        // BinaryImage thresholdImage = frame.thresholdHSV(105, 137, 230, 255, 133, 183);C
 
     }
 
-    public static void update() {
+
+    public void getModifiedImage() {
+
 
     }
-
-    public static void getModifiedImage(Mat frame) {
-        //remove image noise
-        Imgproc.blur(frame, HsvImage, new Size(7, 7));
-        //convert to hsv image
-        Imgproc.cvtColor(HsvImage, HsvImage, Imgproc.COLOR_BGR2HSV);
-
-    }
-
 
 //    public void CalculateDist(Target targets) {
 //        //vertical target is 32 inches fixed
