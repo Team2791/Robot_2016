2c6f8b02f11f2cde6a97e80aff0bced13ff8c19f
Akhil Jacob <akhilcjacob@gmail.com>
2016-01-30 12:19:52 -0500

    commit

--- src/org/usfirst/frc/team2791/subsystems/ShakerShooter.java
+++ src/org/usfirst/frc/team2791/subsystems/ShakerShooter.java
@@ -8,107 +8,105 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import org.usfirst.frc.team2791.configuration.Constants;
 import org.usfirst.frc.team2791.configuration.Ports;
 
-
 public class ShakerShooter extends ShakerSubsystem {
-    private CANTalon leftShooterTalon;
-    private CANTalon rightShooterTalon;
-    private ShooterHeight shooterState;
-    private Solenoid firstLevelSolenoid;
-    private Solenoid secondLevelSolenoid;
-    private boolean robotHasBall;
-    private Servo ballAidServo;
+	private CANTalon leftShooterTalon;
+	private CANTalon rightShooterTalon;
+	private ShooterHeight shooterState;
+	private Solenoid firstLevelSolenoid;
+	private Solenoid secondLevelSolenoid;
+	private boolean robotHasBall;
+	private Servo ballAidServo;
 
-    public ShakerShooter() {
-        init();
-    }
+	public ShakerShooter() {
+		leftShooterTalon = new CANTalon(Ports.SHOOTER_TALON_LEFT_PORT);
+		rightShooterTalon = new CANTalon(Ports.SHOOTER_TALON_RIGHT_PORT);
+		leftShooterTalon.setInverted(true);
+		firstLevelSolenoid = new Solenoid(Ports.PCM_MODULE, Ports.SHOOTER_PISTON_CHANNEL_FIRST_LEVEL);
+		secondLevelSolenoid = new Solenoid(Ports.PCM_MODULE, Ports.SHOOTER_PISTON_CHANNEL_SECOND_LEVEL);
+		robotHasBall = false;
+		ballAidServo = new Servo(Ports.BALL_AID_SERVO_PORT);
+	}
 
-    public void init() {
-        leftShooterTalon = new CANTalon(Ports.SHOOTER_TALON_LEFT_PORT);
-        rightShooterTalon = new CANTalon(Ports.SHOOTER_TALON_RIGHT_PORT);
-        leftShooterTalon.setInverted(true);
-        firstLevelSolenoid = new Solenoid(Ports.PCM_MODULE, Ports.SHOOTER_PISTON_CHANNEL_FIRST_LEVEL);
-        secondLevelSolenoid = new Solenoid(Ports.PCM_MODULE, Ports.SHOOTER_PISTON_CHANNEL_SECOND_LEVEL);
-        robotHasBall = false;
-        ballAidServo = new Servo(Ports.BALL_AID_SERVO_PORT);
-    }
+	public void run() {
+		leftShooterTalon.set(Constants.SHOOTER_SPEED);
+		rightShooterTalon.set(Constants.SHOOTER_SPEED);
+	}
 
-    public void run() {
-        leftShooterTalon.set(Constants.SHOOTER_SPEED);
-        rightShooterTalon.set(Constants.SHOOTER_SPEED);
-    }
+	public void run(double syncedSpeed) {
+		leftShooterTalon.set(syncedSpeed);
+		rightShooterTalon.set(syncedSpeed);
+	}
 
-    public void run(double syncedSpeed) {
-        leftShooterTalon.set(syncedSpeed);
-        rightShooterTalon.set(syncedSpeed);
-    }
+	public void run(double leftSpeed, double rightSpeed) {
+		leftShooterTalon.set(leftSpeed);
+		rightShooterTalon.set(rightSpeed);
+	}
 
-    public void run(double leftSpeed, double rightSpeed) {
-        leftShooterTalon.set(leftSpeed);
-        rightShooterTalon.set(rightSpeed);
-    }
+	public void disable() {
+		stopMotors();
+	}
 
-    public void disable() {
-        stopMotors();
-    }
+	public void reset() {
+		stopMotors();
+		setShooterLow();
+	}
 
-    public void reset() {
-        stopMotors();
-        setShooterLow();
-    }
+	public void update() {
+		SmartDashboard.putString("Shooter Height: ", getShooterHeight().toString());
+	}
 
-    public void update() {
-        SmartDashboard.putString("Shooter Height: ", getShooterHeight().toString());
-    }
+	public ShooterHeight getShooterHeight() {
+		refreshShooterHeight();
+		return shooterState;
+	}
 
-    public ShooterHeight getShooterHeight() {
-        refreshShooterHeight();
-        return shooterState;
-    }
+	public void refreshShooterHeight() {
+		if (firstLevelSolenoid.get() && secondLevelSolenoid.get())
+			shooterState = ShooterHeight.HIGH;
+		else if (firstLevelSolenoid.get())
+			shooterState = ShooterHeight.MID;
+		else
+			shooterState = ShooterHeight.LOW;
 
-    public void refreshShooterHeight() {
-        if (firstLevelSolenoid.get() && secondLevelSolenoid.get())
-            shooterState = ShooterHeight.HIGH;
-        else if (firstLevelSolenoid.get())
-            shooterState = ShooterHeight.MID;
-        else shooterState = ShooterHeight.LOW;
+	}
 
-    }
+	public void setShooterLow() {
+		shooterState = ShooterHeight.LOW;
+		firstLevelSolenoid.set(Constants.SHOOTER_LOW_STATE);
+		secondLevelSolenoid.set(Constants.SHOOTER_LOW_STATE);
+	}
 
-    public void setShooterLow() {
-        shooterState = ShooterHeight.LOW;
-        firstLevelSolenoid.set(Constants.SHOOTER_LOW_STATE);
-        secondLevelSolenoid.set(Constants.SHOOTER_LOW_STATE);
-    }
+	public void setShooterMiddle() {
+		shooterState = ShooterHeight.MID;
+		firstLevelSolenoid.set(Constants.SHOOTER_HIGH_STATE);
+		secondLevelSolenoid.set(Constants.SHOOTER_LOW_STATE);
+	}
 
-    public void setShooterMiddle() {
-        shooterState = ShooterHeight.MID;
-        firstLevelSolenoid.set(Constants.SHOOTER_HIGH_STATE);
-        secondLevelSolenoid.set(Constants.SHOOTER_LOW_STATE);
-    }
+	public void setShooterHigh() {
+		shooterState = ShooterHeight.HIGH;
+		firstLevelSolenoid.set(Constants.SHOOTER_HIGH_STATE);
+		secondLevelSolenoid.set(Constants.SHOOTER_HIGH_STATE);
+	}
 
-    public void setShooterHigh() {
-        shooterState = ShooterHeight.HIGH;
-        firstLevelSolenoid.set(Constants.SHOOTER_HIGH_STATE);
-        secondLevelSolenoid.set(Constants.SHOOTER_HIGH_STATE);
-    }
+	public boolean hasBall() {
+		return robotHasBall;
+	}
 
-    public boolean hasBall() {
-        return robotHasBall;
-    }
+	public void pushBall() {
+		ballAidServo.setAngle(Constants.SERVO_PUSH_ANGLE);
+	}
 
-    public void pushBall() {
-    	ballAidServo.setAngle(Constants.SERVO_PUSH_ANGLE);
-    }
-    public void resetServoAngle(){
-    	ballAidServo.setAngle(Constants.SERVO_DEFAULT_ANGLE);
-    }
-    public void stopMotors() {
-        leftShooterTalon.set(0.0);
-        rightShooterTalon.set(0.0);
-    }
+	public void resetServoAngle() {
+		ballAidServo.setAngle(Constants.SERVO_DEFAULT_ANGLE);
+	}
 
-    public enum ShooterHeight {
-        LOW, MID, HIGH
-    }
+	public void stopMotors() {
+		leftShooterTalon.set(0.0);
+		rightShooterTalon.set(0.0);
+	}
+
+	public enum ShooterHeight {
+		LOW, MID, HIGH
+	}
 
 }
--- src/org/usfirst/frc/team2791/subsystems/ShakerSubsystem.java
+++ src/org/usfirst/frc/team2791/subsystems/ShakerSubsystem.java
@@ -2,9 +2,7 @@ package org.usfirst.frc.team2791.subsystems;
 
 public abstract class ShakerSubsystem {
 
-    abstract protected void init();
-
-    abstract public void run();
+	abstract public void run();
 
     abstract public void update();
 
