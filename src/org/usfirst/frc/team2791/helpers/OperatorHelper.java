91af80cb32eb3131593d46b3042472a8cda4f597
Akhil Jacob <akhilcjacob@gmail.com>
2016-02-09 16:11:30 -0500

    Button Toggles

--- src/org/usfirst/frc/team2791/helpers/OperatorHelper.java
+++ src/org/usfirst/frc/team2791/helpers/OperatorHelper.java
@@ -3,66 +3,93 @@ package org.usfirst.frc.team2791.helpers;
 import static org.usfirst.frc.team2791.robot.Robot.*;
 
 public class OperatorHelper extends ShakerHelper {
-    private double whenShotBall = 0;
-    private boolean shooterIsReset = false;
-    private int shooterSpeedIndex = 0;
-    private boolean useShooterPID = false;
+	private double whenShotBall = 0;
+	private boolean shooterIsReset = false;
+	private int shooterSpeedIndex = 0;
+	private boolean useShooterPID = false;
+	private boolean extendIntake = false;
+	private boolean useArmAttachment = false;
+	private int shooterLevel = 0;
 
-    public OperatorHelper() {
-        // init
-    }
+	public OperatorHelper() {
+		// init
+	}
 
-    public void teleopRun() {
-        // Operator button layout
-        if (operatorJoystick.getButtonB()) {
-            //Run intake inward with assistance of the shooter wheel
-            shooter.setShooterSpeeds(-1, false);
-            intake.pullBall();
-        } else if (operatorJoystick.getButtonX()) {
-            //Run reverse if button pressed
-            shooter.setShooterSpeeds(1, false);
-            intake.pushBall();
-        } else {
-            //else run the manual controls
-            shooter.setShooterSpeeds(operatorJoystick.getAxisRT() - operatorJoystick.getAxisLT(), useShooterPID);
-            intake.stopMotors();
-        }
-        //Extend intake
-        if (operatorJoystick.getButtonA())
-            intake.extendIntake();
-        //Retract intake
-        if (operatorJoystick.getButtonY())
-            intake.retractIntake();
-        //autofire shooter
-        if (operatorJoystick.getDpadUp())
-            shooter.autoFire(1.0);//currently only runs the servo back and forth
+	public void teleopRun() {
+		// Operator button layout
+		if (operatorJoystick.getButtonB()) {
+			// Run intake inward with assistance of the shooter wheel
+			shooter.setShooterSpeeds(-1  , false);
+			intake.pullBall();
+		} else if (operatorJoystick.getButtonX()) {
+			// Run reverse if button pressed
+			shooter.setShooterSpeeds(1, false);
+			intake.pushBall();
+		} else {
+			// else run the manual controls
+			shooter.setShooterSpeeds(operatorJoystick.getAxisRT() - operatorJoystick.getAxisLT(), useShooterPID);
+			intake.stopMotors();
+		}
 
-        //movement of arm attachment
-        if (operatorJoystick.getDpadRight())
-            intake.setArmAttachmentUp();
-        if (operatorJoystick.getDpadLeft())
-            intake.setArmAttachmentDown();
-        // Start button to reset to teleop start
-        if (operatorJoystick.getButtonSt())
-            reset();
-        if (operatorJoystick.getButtonSel())
-            useShooterPID = !useShooterPID;
-    }
+		if (extendIntake)
+			// Extend intake
+			intake.extendIntake();
+		else
+			// Retract intake
+			intake.retractIntake();
 
-    public void disableRun() {
-        intake.disable();
-        shooter.disable();
-    }
+		// autofire shooter
+		if (operatorJoystick.getDpadUp())
+			shooter.autoFire(1.0);// currently only runs the servo back and
+									// forth
+		// toggle arm attachments
+		if (useArmAttachment)
+			intake.setArmAttachmentDown();
+		else
+			intake.setArmAttachmentUp();
+		switch (shooterLevel) {
+		case 0:
+			shooter.setShooterLow();
+		case 1:
+			shooter.setShooterMiddle();
+		case 2:
+			shooter.setShooterHigh();
+		}
 
-    public void updateSmartDash() {
-        intake.updateSmartDash();
-        shooter.updateSmartDash();
-    }
+		// Start button to reset to teleop start
+		if (operatorJoystick.getButtonSt())
+			reset();
+		// toggle the pid
+		if (operatorJoystick.getButtonSel())
+			useShooterPID = !useShooterPID;
+		// intake extension toggle
+		if (operatorJoystick.getButtonA())
+			extendIntake = !extendIntake;
+		// arm attachment
+		if (operatorJoystick.getButtonY())
+			useArmAttachment = !useArmAttachment;
+		if (operatorJoystick.getButtonRB())
+			shooterLevel++;
+		if (operatorJoystick.getButtonLB())
+			shooterLevel--;
+		shooterLevel = shooterLevel > 2 ? 2 : shooterLevel;
+		shooterLevel = shooterLevel < 0 ? 0 : shooterLevel;
+	}
 
-    public void reset() {
-        shooterSpeedIndex = 0;
-        shooter.reset();
-        intake.reset();
-    }
+	public void disableRun() {
+		intake.disable();
+		shooter.disable();
+	}
+
+	public void updateSmartDash() {
+		intake.updateSmartDash();
+		shooter.updateSmartDash();
+	}
+
+	public void reset() {
+		shooterSpeedIndex = 0;
+		shooter.reset();
+		intake.reset();
+	}
 
 }
