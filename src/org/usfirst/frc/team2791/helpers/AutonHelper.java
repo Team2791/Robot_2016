2d04f590ccf6d75b2223b89a0d363463db363d15
Akhil Jacob <akhilcjacob@users.noreply.github.com>
2016-02-19 10:30:36 -0500

    added some methods to cross terrain defenses
    I did this without an ide so there will most likely be errors

--- src/org/usfirst/frc/team2791/helpers/AutonHelper.java
+++ src/org/usfirst/frc/team2791/helpers/AutonHelper.java
@@ -13,7 +13,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class AutonHelper extends ShakerHelper {
 	private int counter = 0;
 	private double setPoint = 0;
-
+	private double lowBar_sweetSpotTime = 0 ;
 	public AutonHelper() {
 
 		SmartDashboard.putNumber("Angle P", PID.DRIVE_ANGLE_P);
@@ -45,18 +45,10 @@ public class AutonHelper extends ShakerHelper {
 			break;
 		case 1:
 			driveTrain.driveInFeet(SmartDashboard.getNumber("pid distance travel"), 
-					SmartDashboard.getNumber("Angle setpoint"));
-//			driveTrain.setAngle(SmartDashboard.getNumber("Angle setpoint")/12);
+					SmartDashboard.getNumber("Angle setpoint"),0.4);
 			
 //		case 1:
-//			SmartDashboard.getNumber("pid distance travel")
-//			if (!driveTrain.driveInFeet(18*12, 0)) {
-//				pidSweetSpotEnterTime = Timer.getFPGATimestamp();
-//			}
-//				
-//			if(Timer.getFPGATimestamp() - pidSweetSpotEnterTime > 0.5)
-//				counter++;
-//			break;
+//		traverseLowBar();
 //		case 2:
 ////			SmartDashboard.getNumber("pid distance travel")
 //			if (!driveTrain.setAngle(60/12)) {
@@ -71,7 +63,7 @@ public class AutonHelper extends ShakerHelper {
 //			break;
 //		case 3:
 ////			SmartDashboard.getNumber("pid distance travel")
-//			if (!driveTrain.driveInFeet(5*12, 60/12)) {
+//			if (!driveTrain.driveInFeet(5, 60/12)) {
 //				pidSweetSpotEnterTime = Timer.getFPGATimestamp();
 //			}
 //				
@@ -108,4 +100,34 @@ public class AutonHelper extends ShakerHelper {
 	public void reset() {
 
 	}
+	public boolean traverseLowBar(){//This should be the distance from the neutral zone to right after the low bar
+	//it waits for the pid to be good for at least 0.5 seconds before giving true
+		intake.extendIntake();
+		intake.setArmAttachmentDown();
+		if (!driveTrain.driveInFeet(18, 0,0.5)) {
+			lowBar_sweetSpotTime = Timer.getFPGATimestamp();
+		}	
+		if(Timer.getFPGATimestamp() - lowBar_sweetSpotTime > 0.5)
+			return true;
+		return false;
+	}
+	public boolean traverseUnevenTerrain{
+		//follows the same principle as traverse low bar but allows for greater output for pid
+		intake.retractIntake();
+		intake.setArmAttachmentUp();
+		if (!driveTrain.driveInFeet(18, 0,0.7)) {
+			lowBar_sweetSpotTime = Timer.getFPGATimestamp();
+		}	
+		if(Timer.getFPGATimestamp() - lowBar_sweetSpotTime > 0.5)
+			return true;
+		return false;
+		
+	}
+	public enum defense{	//uneven terrain applies to rough terrain,moat, etc. ... things that challenge our drive train
+		LOW_BAR, UNEVEN_TERRAIN
+	}
+	public enum autonPosition{// this is used to determine how the robot should act after crossing the defense
+		ONE,TWO,THREE,FOUR,FIVE
+	}
+	
 }
