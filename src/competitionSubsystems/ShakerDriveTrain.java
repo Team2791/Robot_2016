eb6522576d78c1e5df1180f6584a59e9f5e68bbd
Max Llewellyn <max.llewellyn@gmail.com>
2016-03-15 17:14:08 -0400

    Started to rework subsystems into abstract classes. Still a work in progress.

---
+++ src/competitionSubsystems/ShakerDriveTrain.java
@@ -0,0 +1,7 @@
+package competitionSubsystems;
+
+import org.usfirst.frc.team2791.abstractSubsystems.AbstractShakerDriveTrain;
+
+public class ShakerDriveTrain extends AbstractShakerDriveTrain {
+
+}
