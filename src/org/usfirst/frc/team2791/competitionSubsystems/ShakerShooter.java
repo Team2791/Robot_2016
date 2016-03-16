0cd7284da6eaf521f2b7415b59ab6899482d1af0
Akhil Jacob <akhilcjacob@gmail.com>
2016-03-16 14:42:59 -0400

    Finished implementation of the abstract classes in the CompBot and has also been tested

--- src/org/usfirst/frc/team2791/competitionSubsystems/ShakerShooter.java
+++ src/org/usfirst/frc/team2791/competitionSubsystems/ShakerShooter.java
@@ -1,55 +1,63 @@
 package org.usfirst.frc.team2791.competitionSubsystems;
 
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Servo;
+
 import org.usfirst.frc.team2791.abstractSubsystems.AbstractShakerShooter;
 import org.usfirst.frc.team2791.util.Constants;
 
 public class ShakerShooter extends AbstractShakerShooter {
-    private DoubleSolenoid shortPiston;
-    private DoubleSolenoid longPiston;
+	private DoubleSolenoid shortPiston;
+	private DoubleSolenoid longPiston;
 
-    public ShakerShooter() {
-        super();
-        longPiston = new DoubleSolenoid(Constants.PCM_MODULE, Constants.LONG_PISTON_FORWARD,
-                Constants.LONG_PISTON_REVERSE);
-        shortPiston = new DoubleSolenoid(Constants.PCM_MODULE, Constants.SHORT_PISTON_FORWARD,
-                Constants.SHORT_PISTON_REVERSE);
-        init();
-    }
+	public ShakerShooter() {
+		super();
+		longPiston = new DoubleSolenoid(Constants.PCM_MODULE, Constants.LONG_PISTON_FORWARD,
+				Constants.LONG_PISTON_REVERSE);
+		shortPiston = new DoubleSolenoid(Constants.PCM_MODULE, Constants.SHORT_PISTON_FORWARD,
+				Constants.SHORT_PISTON_REVERSE);
+		leftShooterTalon = new CANTalon(Constants.SHOOTER_TALON_LEFT_PORT);
+		rightShooterTalon = new CANTalon(Constants.SHOOTER_TALON_RIGHT_PORT);
+		ballAidServo = new Servo(Constants.BALL_AID_SERVO_PORT);
+		ballDistanceSensor = new AnalogInput(Constants.BALL_DISTANCE_SENSOR_PORT);
 
+		init();
+	}
 
-    public ShooterHeight getShooterHeight() {
-        // get current shooter height by determining which solenoid are true
-        if (shortPiston.get().equals(Constants.SMALL_PISTON_HIGH_STATE)
-                && longPiston.get().equals(Constants.LARGE_PISTON_HIGH_STATE))
-            return ShooterHeight.HIGH;
-        else if (longPiston.get().equals(Constants.LARGE_PISTON_HIGH_STATE))
-            return ShooterHeight.MID;
-        else
-            return ShooterHeight.LOW;
+	public ShooterHeight getShooterHeight() {
+		// get current shooter height by determining which solenoid are true
+		if (shortPiston.get().equals(Constants.SMALL_PISTON_HIGH_STATE)
+				&& longPiston.get().equals(Constants.LARGE_PISTON_HIGH_STATE))
+			return ShooterHeight.HIGH;
+		else if (longPiston.get().equals(Constants.LARGE_PISTON_HIGH_STATE))
+			return ShooterHeight.MID;
+		else
+			return ShooterHeight.LOW;
 
-    }
+	}
 
-    protected void moveShooterPistonsLow() {
-        // both pistons will be set to true to get max height
-        // both pistons will be set to true to get low height
-        shortPiston.set(Constants.SMALL_PISTON_HIGH_STATE); // was reverse
-        // //this is short
-        // one
-        longPiston.set(Constants.LARGE_PISTON_HIGH_STATE);
-    }
+	protected void moveShooterPistonsLow() {
+		// both pistons will be set to true to get max height
+		// both pistons will be set to true to get low height
+		shortPiston.set(Constants.SMALL_PISTON_HIGH_STATE); // was reverse
+		// //this is short
+		// one
+		longPiston.set(Constants.LARGE_PISTON_HIGH_STATE);
+	}
 
-    protected void moveShooterPistonsMiddle() {
-        // set shooter height to middle meaning only one piston will be true
-        shortPiston.set(Constants.SMALL_PISTON_LOW_STATE);
-        longPiston.set(Constants.LARGE_PISTON_HIGH_STATE);
-    }
+	protected void moveShooterPistonsMiddle() {
+		// set shooter height to middle meaning only one piston will be true
+		shortPiston.set(Constants.SMALL_PISTON_LOW_STATE);
+		longPiston.set(Constants.LARGE_PISTON_HIGH_STATE);
+	}
 
-    protected void moveShooterPistonsHigh() {
-        // set shooter height to low , set both pistons to false
-        shortPiston.set(Constants.SMALL_PISTON_LOW_STATE);
-        longPiston.set(Constants.LARGE_PISTON_LOW_STATE);
-        // short needs to switch
-    }
+	protected void moveShooterPistonsHigh() {
+		// set shooter height to low , set both pistons to false
+		shortPiston.set(Constants.SMALL_PISTON_LOW_STATE);
+		longPiston.set(Constants.LARGE_PISTON_LOW_STATE);
+		// short needs to switch
+	}
 
 }
